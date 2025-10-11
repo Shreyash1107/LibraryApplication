2db970cb1e77536dc071cf957201ad01f6abdc90
Shreyash Kawane <shreyunkawane@gmail.com>
2025-10-11 10:00:59 +0530

    Initial Commit for Library Management System

---
+++ src/main/java/com/org/LibraryApplication/LibraryApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.org.LibraryApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/LibraryApplication/LibraryApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.LibraryApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
