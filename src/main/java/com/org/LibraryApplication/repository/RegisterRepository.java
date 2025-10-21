721ee49ca6177a304ff53f35fca09458c9429416
Shreyash Kawane <shreyunkawane@gmail.com>
2025-10-21 22:08:20 +0530

    Working on Login Functionality

--- src/main/java/com/org/LibraryApplication/repository/RegisterRepository.java
+++ src/main/java/com/org/LibraryApplication/repository/RegisterRepository.java
@@ -2,6 +2,8 @@ package com.org.LibraryApplication.repository;
 
 import com.org.LibraryApplication.entity.RegisterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +11,6 @@ public interface RegisterRepository extends JpaRepository<RegisterEntity,Integer
     boolean existsByEmail(String email);
     boolean existsByContact(String contact);
     boolean existsByRolesAssignedId(Integer id);
+    @Query("select reg from RegisterEntity where reg.email =: email and reg.password =: password")
+    boolean login(@Param("email") String email, @Param("password") String password);
 }
